a6e708b5326b7ea5817bbc2e6e934d0dea625326
Levente Bognar <lajos93@users.noreply.github.com>
2021-10-20 16:01:53 +0200

    test

--- routes/test.js
+++ routes/test.js
@@ -4,10 +4,13 @@ const express = require("express");
 const routes = express.Router();
 
 routes.get("/", (req, res) => {
-  const url = "https://www.szakkatalogus.hu/adat/BABI-K%C5%90_BT-714810";
+  const url =
+    "https://www.szakkatalogus.hu/adat/STADLER_%C3%9CVEGCSISZOL%C3%93_%C3%89S_GRAV%C3%8DROZ%C3%93_M%C3%9BHELY-716961";
+  const url2 =
+    "https://www.szakkatalogus.hu/adat/STADLER_%C3%9CVEGCSISZOL%C3%93_%C3%89S_GRAV%C3%8DROZ%C3%93_M%C5%B0HELY-716961";
 
   axios
-    .get(url, { responseType: "arraybuffer" })
+    .get(url2, { responseType: "arraybuffer" })
     .then(function (response) {
       //get page links
       console.log(response.data);
