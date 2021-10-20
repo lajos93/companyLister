7bdb9b94ad121d296d34e76f4288b0441ffb82ad
Levente Bognar <lajos93@users.noreply.github.com>
2021-10-20 12:46:46 +0200

    cleanup

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const express = require("express");
 const app = express();
 const data = require("./routes/data");
-const test = require("./routes/test");
 
 app.use(express.json());
 app.use("/search/", data);
-app.use("/test/", test);
 const port = 3001;
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
